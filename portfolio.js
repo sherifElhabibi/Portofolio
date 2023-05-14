7352c0cc64b5f35e7151e8ba134d9b1d1445e6cd
sherifElhabibi <sherif.hussein58@gmail.com>
2023-05-14 05:25:08 +0300

    first commit

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+let nav = document.querySelectorAll(".navmenu-parent")[0];
+let navSmall = document.querySelectorAll(".navmenu-small")[0];
+
+let typed = new Typed(".typed", {
+    strings: ["Front-End Developer", ".NET Full-Stack Developer"],
+    typeSpeed: 30,
+    backSpeed: 30,
+    loop: true,
+  });
